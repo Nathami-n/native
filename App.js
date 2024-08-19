f6865e45bd797d07c31fdfa1b9db910121fb3f09
Nathami-n <georgenathan010@gmail.com>
2024-08-19 22:54:47 +0300

    first app

--- App.js
+++ App.js
@@ -114,7 +114,7 @@ export default function App() {
         <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
           <Emojilist onSelect={setPickedEmoji} onCloseModal={onModalClose} />
         </EmojiPicker>
-        <StatusBar style="auto" />
+        <StatusBar style="light" />
       </View>
     </GestureHandlerRootView>
   );
