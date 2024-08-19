cd18ebe3df9946bfcfc466eea178f7478f97e239
Nathami-n <georgenathan010@gmail.com>
2024-08-19 11:46:48 +0300

    add: the modal

--- App.js
+++ App.js
@@ -6,12 +6,14 @@ import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import IconButton from "./components/icon-button";
 import CircleButton from "./components/circle-button";
+import EmojiPicker from "./components/emoji-picker";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(true);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -33,6 +35,11 @@ export default function App() {
 
   const onAddSticker = () => {
     // we will implement this later
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
   };
 
   const onSaveImageAsync = async () => {
@@ -71,6 +78,11 @@ export default function App() {
           />
         </View>
       )}
+
+      <EmojiPicker
+        isVisible={isModalVisible}
+        onClose={onModalClose}
+      ></EmojiPicker>
       <StatusBar style="auto" />
     </View>
   );
@@ -97,6 +109,6 @@ const styles = StyleSheet.create({
   },
   optionsRow: {
     alignItems: "center",
-    flexDirection: "row"
-  }
+    flexDirection: "row",
+  },
 });
